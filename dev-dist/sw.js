0afd92b7eab161ea914435c29cde847a2e7d993e
AutoMotive <hbk.hrishi0412@gmail.com>
2025-10-21 23:40:45 +0530

    Fix app issues - Add PNG icon files and increase PWA file size limit to resolve build errors

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0ta121aleok"
+    "revision": "0.p3sf1dhs6f8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
