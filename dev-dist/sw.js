1b8f40a6d6388dba3453c37156f1c3adf79cbca2
AutoMotive <hbk.hrishi0412@gmail.com>
2025-10-23 00:10:24 +0530

    Update service worker (sw.js) for PWA functionality

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7s8qo0n0rgk"
+    "revision": "0.uhbn354haio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
